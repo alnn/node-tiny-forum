ff350745533c557d50670943e214bbe9658dbcd1
ernium <ernium@gmail.com>
2016-11-10 23:16:02 +0300

    change heroku mongo options

--- config/heroku.js
+++ config/heroku.js
@@ -5,12 +5,8 @@ module.exports = {
   mongoose: {
     uri: 'mongodb://node-tiny-forum:sd8fhdf8ka334sd2j37x34@ds037617.mlab.com:37617/node-tiny-forum',
     options: {
-      server: {
-        socketOptions: {
-          keepAlive: 1
-        },
-        poolSize: 5
-      }
+      server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+      replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } }
     }
   },
   port: 9000
